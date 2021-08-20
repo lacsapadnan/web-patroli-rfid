d2f3af921ba8bdc845e02dbbd4721f9d1c541c2b
lacsapadnan <pascaladnan101@gmail.com>
2021-08-20 13:37:16 +0700

    add profile update

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -22,6 +22,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
+            'address' => ['nullable', 'string', 'max:255'],
+            'phone' => ['nullable', 'string', 'max:255'],
         ])->validateWithBag('updateProfileInformation');
 
         if (isset($input['photo'])) {
@@ -35,6 +37,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'address' => $input['address'],
+                'phone' => $input['phone'],
             ])->save();
         }
     }
@@ -51,6 +55,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'address' => $input['address'],
+            'phone' => $input['phone'],
             'email_verified_at' => null,
         ])->save();
 
--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('rfid', RfidController::class);
 Route::post('mulai-patroli', [PatroliController::class,'mulaiPatroli']);
-
 Route::get('get-uuid', [UUIDController::class, 'getUUID']);
